aee62cab7b71109352062b75a9a62af4402e5149
alexyun <alexyun@castis.com>
2014-11-05 14:17:35 +0900

    cpplint 설치 버전을 최신으로

--- install_cpptools.sh
+++ install_cpptools.sh
@@ -4,7 +4,9 @@ cd ~
 wget https://bootstrap.pypa.io/get-pip.py --no-check-certificate
 python get-pip.py
 pip install gcovr
-pip install cpplint
 rm -rf get-pip.py
 
+wget http://google-styleguide.googlecode.com/svn/trunk/cpplint/cpplint.py
+install cpplint.py /usr/local/bin/cpplint
+rm -rf cpplint.py
 
